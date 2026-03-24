4855585bb5bf124fceaf32fcf17d1ccf0e5d8457
SDV-G-Deploy <sergei.dvoriankin@gmail.com>
2026-03-24 15:09:41 +0300

    Add Firebase configuration to firebase-config.js
    Added Firebase configuration with necessary keys.

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
-// Firebase config не настроен.
-// Скопируйте firebase-config.template.js в firebase-config.js и заполните значения.
-window.TRACKSUBFIN_FIREBASE_CONFIG = null;
+window.TRACKSUBFIN_FIREBASE_CONFIG = {
+apiKey: "AIzaSyAXxB2s4__wfGsoZjKiW10fELPoY2MiyOI",
+authDomain: "tracksubfin.firebaseapp.com",
+projectId: "tracksubfin",
+storageBucket: "tracksubfin.firebasestorage.app",
+messagingSenderId: "1021388819923",
+appId: "1:1021388819923:web:f1ce48acf3009ef576f326",
+measurementId: "G-SX59YJ6VL3",
+};
